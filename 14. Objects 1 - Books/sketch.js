fdc70334e969074ea3ad3cd7fa16b6db4f929377
Noah-CS30 <a68518055@gmail.com>
2026-03-24 12:16:29 -0600

    14. Objects - book

---
+++ 14. Objects 1 - Books/sketch.js
@@ -0,0 +1,62 @@
+// Objects Books
+// Noah J
+
+let myBook; // you cannot init objects here
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  myBook = new Book("CS30 Text", "Noah",
+    7013326, "leatherbound" , 500,
+    width * 0.3);
+ 
+}
+
+
+
+
+class Book{
+  // 1. constructor 
+  constructor(title, author, isbn, cover, pages, x ){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.cover = cover;
+    this.pages = pages;
+    this.x = x;
+ 
+
+
+    //2. Class Methods
+    // since we're in a class, we omit
+    // function keyword
+    display(){
+      //render our book object on Canvas
+      rectMode(CENTER); textAlign(CENTER, CENTER);
+      textSize(20);
+    }
+
+    // set fill color based on covertype
+    switch(this.cover){
+      case "softcover":
+        fill(250,200,150); break;
+      case "softcover":
+        fill(250,200,150); break;
+      case "leatherbound":
+        fill(250,200,150); break;
+    }
+
+    // now draw the book
+    push();
+    translate(this.x, height/2);
+    rect(this.x, height/2, this.pages/10,150);
+    fill(255);
+    text(this.title[0],0,-50);
+    pop();
+
+   
+  }
+}
+function draw() {
+  background(220);
+  myBook.display();
+}
--- Monday challenge/sketch.js
+++ Monday challenge/sketch.js
@@ -1,15 +1,56 @@
-// Project Title
+// Monday challenge 
 // Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+let items = [];
+let provinces = new map();
+let currentP = "SK";
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(200, 200);
+
+  for (let i = 0; i < 20; i++){
+    let item = {
+      x: random(width - 60),
+      y: random(height),
+      speedY: random(1,3),
+      basePrice : random(10,100),
+      name : "Item" + i
+    };
+    items.push(item);
+  provinces.set("SK", {tax: 1.11});
+  provinces.set("AB", {tax: 1.05});
+  provinces.set("ON", {tax: 1.13})
+  }
 }
 
-function draw() {
-  background(220);
+function draw(){
+  background(230);
+
+  let rules = provinces.get(currentP);
+
+  for( let i = 0; i < items.length; i++){
+    items[i].y += items[i].speedY;
+    items[i].y = 0;
+  }
+
+  fill(255);
+  rect(items[i].x, items[i].y, 60, 40)
+
+  fill(0);
+  textAlign(CENTER, CENTER);
+  text(items[i].name, items[i].x + 30, items[i].y + 30);
+  let totalP = items[i].basePrice * rules.tax;
+  
+}
+
+function keyPressed() { 
+  if(key === '1'){
+    currentP = "SK";
+  }
+  else if(key === '2'){
+    currentP = "AB";
+  }
+  else if(key === '3'){
+    currentP = "ON";
+  }
 }

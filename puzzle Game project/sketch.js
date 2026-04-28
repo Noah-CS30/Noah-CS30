6714403805d86e1c9c49a2a8f09a1becabaf9e35
Noah-CS30 <a68518055@gmail.com>
2026-04-28 10:48:33 -0600

    Initialize grid and set canvas size for puzzle game
    Updated project metadata and initialized grid for puzzle game.

--- puzzle Game project/sketch.js
+++ puzzle Game project/sketch.js
@@ -1,13 +1,21 @@
 // Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Noah
+// basic variables
+let grid = [];
+let cols = 5;
+let rows = 5;
+let cellS;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 500);
+  // intialize the board( zero = black )
+  cellS = width / cols;
+  for (let i = 0; i < cols; i++){
+    grid[i] = [];
+      for (let k = 0; k < rows; k++){
+        grid = [i][j] = 0
+      }
 }
 
 function draw() {

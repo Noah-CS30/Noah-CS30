9ca7e93b000a2f36ac7bbb2bcf79e69b55d6a80f
Noah-CS30 <a68518055@gmail.com>
2026-04-28 12:17:41 -0600

    practice

---
+++ Image Manipulation Exercises 4/sketch.js
@@ -0,0 +1,19 @@
+let pixel = [];
+let myImage;
+
+function preload(){
+  myImage = loadImage("assets/hand.jpg");
+}
+
+function setup() {
+  createCanvas(650, 650);
+}
+
+function draw() {
+  background(220);
+  image(myImage,0,0);
+  loadPixels();
+  for(let i = 0; i < pixels.length; i += 4){
+    
+  }
+}
--- Image Manipulation exercises 2/sketch.js
+++ Image Manipulation exercises 2/sketch.js
@@ -1,15 +1,32 @@
 // Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+let pixel = [];
+let myImage;
+
+function preload(){
+  myImage = loadImage("assets/race.jpg");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(myImage.width, myImage.height);
+  noLoop();
 }
 
 function draw() {
   background(220);
+  image(myImage, 0,0);
+  loadPixels();
+  for(let i = 0; i < myImage.width; x++){
+    for(let k = 0; k < myImage.height; k++){
+      if ( i >= width / 2){
+        let index = ( i + k * width) * 4;
+        pixels[index + 1] = 0;
+      }
+    }
+  }
+
+  myImage.updatePixels();
+
+
+
 }
